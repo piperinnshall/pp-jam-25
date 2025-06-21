2a925913bb24067cea63ee19dd54d426ba09e14b
Filkspline <LeoRiginelli@gmail.com>
2025-06-21 23:27:41 +1200

    chore: added lights

--- src/scripts/player/player.gd
+++ src/scripts/player/player.gd
@@ -11,7 +11,7 @@ var hooked = false
 func _ready():
 	# Set the initial emoticon face
 	label.text = "0 . 0"
-
+	label.modulate = Color.BLACK
 func _process(delta: float) -> void:
 	print(rad_to_deg(get_angle_to(get_global_mouse_position())))
 	
